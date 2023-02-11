16d2e84e5c332491ca0f231b0cca0703c66d98dc
nhattuan44 <nhattuan44t@gmail.com>
2023-02-11 12:27:04 +0700

    Add-Decentralization

--- Student/studentViews.py
+++ Student/studentViews.py
@@ -4,8 +4,11 @@ from .studentModels import Student
 from .studentSerializer import StudentSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from StudentManager.roleLoginDecorater import RoleRequest
 # Create your views here.
 class StudentsByName(APIView):
+    @method_decorator(RoleRequest(allowedRoles=['NormalUser',]))
     def get(self,request,name):
         students = Student.objects.filter(StudentName__contains=name)
         studentsSerializer = StudentSerializer(students,many=True)
--- StudentManager/settings.py
+++ StudentManager/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'StudentManager.middleware.AuthorizationMiddleware',
 ]
 
 ROOT_URLCONF = 'StudentManager.urls'
